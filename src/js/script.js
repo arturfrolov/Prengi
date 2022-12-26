03c3f0579d61c70355957d1484a6010f54b577a0
Artur <djajikamail@gmail.com>
2022-12-26 15:23:32 +0200

    add section solutions, add tabs and slider in this section

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// Slider
+
 $(document).ready(function(){
     $('.promo__slider').slick({
         dots: true,
@@ -19,6 +21,8 @@ $(document).ready(function(){
     });
 });
 
+// Hamburger menu
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.promo__menu');
 const closeElem = document.querySelector('.promo__close');
@@ -36,4 +40,38 @@ menuList.forEach(item => {
   item.addEventListener('click', () => {
     menu.classList.remove('active');
   });
+});
+
+// Slider 2
+
+$(document).ready(function(){
+    $('.solutions__slider').slick({
+        dots: false,
+        // arrows: false,
+        // fade: true,
+        // cssEase: 'linear',
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="prev"><img src="icons/slide/left_mac.svg"></button>',
+        nextArrow: '<button type="button" class="next"><img src="icons/slide/right_mac.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
+
+// Tabs
+
+$(document).ready(function(){
+    $('ul.solutions__tabs').on('click', 'li:not(.solutions__tab_active)', function() {
+      $(this)
+        .addClass('solutions__tab_active').siblings().removeClass('solutions__tab_active')
+        .closest('div.container').find('div.solutions__laptop')
+        .removeClass('solutions__laptop_active').eq($(this).index()).addClass('solutions__laptop_active');
+    });
 });
